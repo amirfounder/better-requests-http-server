0bb7679c9dd30f97ab81000cca59c86f909f83fb
Amir Sharapov <amir@amirfounder.com>
2022-04-20 01:15:25 -0500

    running

--- better_requests/server/services.py
+++ better_requests/server/services.py
@@ -1,18 +1,26 @@
+from daos import GoogleSearchResultsHtmlDocumentRawRepository, GoogleSearchResultsHtmlDocumentIndexRepository
 from daos.internal import NewsArticleHtmlDocumentRawRepository, NewsArticleHtmlDocumentIndexRepository
-from dependency_injection import service
+from dependency_injection import service as ioc_service
 from http_server import HttpMethod
 
 from better_requests.server.adapter import BetterRequestEndpointAdapter
-from better_requests.services.save_news_article_html import SaveNewsArticleHtmlDocument
+from better_requests.services.save_google_results import SaveGoogleSearchResults
+from better_requests.services.save_news_article import SaveNewsArticle
 
 
-# Registering repositories as services in IOC container
 REPOSITORIES = [
-    service(NewsArticleHtmlDocumentRawRepository),
-    service(NewsArticleHtmlDocumentIndexRepository)
+    NewsArticleHtmlDocumentRawRepository,
+    NewsArticleHtmlDocumentIndexRepository,
+    GoogleSearchResultsHtmlDocumentRawRepository,
+    GoogleSearchResultsHtmlDocumentIndexRepository
 ]
 
-# The actual services used by the HTTP server
-SERVICES = [
-    BetterRequestEndpointAdapter('/news-article', HttpMethod.POST, service(SaveNewsArticleHtmlDocument)()),
+for cls in REPOSITORIES:
+    ioc_service(cls)
+
+SERVICE_PARAMS = [
+    ('/save-news-article', HttpMethod.POST, SaveNewsArticle),
+    ('/save-google-search-results', HttpMethod.POST, SaveGoogleSearchResults)
 ]
+
+SERVICES = [BetterRequestEndpointAdapter(r, m, ioc_service(s)()) for r, m, s in SERVICE_PARAMS]
--- better_requests/services/save_news_article.py
+++ better_requests/services/save_news_article.py
@@ -10,7 +10,7 @@ from daos.internal import (
 from better_requests.services.base import BaseBetterRequestsService
 
 
-class SaveNewsArticleHtmlDocument(BaseBetterRequestsService):
+class SaveNewsArticle(BaseBetterRequestsService):
     def __init__(
             self,
             document_repository: NewsArticleHtmlDocumentRawRepository,
@@ -23,20 +23,15 @@ class SaveNewsArticleHtmlDocument(BaseBetterRequestsService):
         document = NewsArticleHtmlDocumentRawModel()
         document.contents = params.get('markup')
 
-        if not document.contents:
-            raise Exception('request body must have markup key.')
-
         self.document_repository.save(document)
 
         index_item = NewsArticleHtmlDocumentIndexModel()
 
         index_item.document_id = document.get_id()
         index_item.document_format = self.document_repository.file_format.value
-
         index_item.raw_version_document_path = str(document.get_path())
         index_item.is_raw_version_stored = True
-
-        index_item.source_url = params.get('url')
+        index_item.url = params.get('url')
         index_item.retrieved_from_web_at = params.get('timestamp')
 
         self.index_repository.save(index_item)
