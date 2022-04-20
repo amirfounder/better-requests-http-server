706a1332312f17c4129fe6addda593649af73e00
Amir Sharapov <amir@amirfounder.com>
2022-04-20 01:57:14 -0500

    saving html again

--- better_requests/services/save_google_results.py
+++ better_requests/services/save_google_results.py
@@ -2,9 +2,7 @@ from typing import Dict
 
 from daos.internal import (
     GoogleSearchResultsHtmlDocumentRawRepository,
-    GoogleSearchResultsHtmlDocumentRawModel,
     GoogleSearchResultsHtmlDocumentIndexRepository,
-    GoogleSearchResultsHtmlDocumentIndexModel
 )
 
 from better_requests.services.base import BaseBetterRequestsService as Base
@@ -13,28 +11,27 @@ from better_requests.services.base import BaseBetterRequestsService as Base
 class SaveGoogleSearchResults(Base):
     def __init__(
             self,
-            document_repository: GoogleSearchResultsHtmlDocumentRawRepository,
+            raw_repository: GoogleSearchResultsHtmlDocumentRawRepository,
             index_repository: GoogleSearchResultsHtmlDocumentIndexRepository
     ):
-        self.document_repository = document_repository
+        self.raw_repository = raw_repository
         self.index_repository = index_repository
 
     def run(self, params: Dict):
-        document = GoogleSearchResultsHtmlDocumentRawModel()
+        document = self.raw_repository.create()
         document.contents = params.get('markup')
 
-        self.document_repository.save(document)
+        self.raw_repository.update(document)
 
-        index_item = GoogleSearchResultsHtmlDocumentIndexModel()
-
+        index_item = self.index_repository.create()
         index_item.document_id = document.id
-        index_item.document_format = self.document_repository.filetype
+        index_item.document_format = self.raw_repository.filetype
         index_item.raw_version_document_path = str(document.path)
         index_item.is_raw_version_stored = True
         index_item.url = params.get('url')
         index_item.retrieved_from_web_at = params.get('timestamp')
 
-        self.index_repository.save(index_item)
+        self.index_repository.update(index_item)
 
         return {
             'status': 'SUCCESS'
