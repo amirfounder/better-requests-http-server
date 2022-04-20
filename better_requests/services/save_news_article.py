c0039cb36a5b924fab5b6779a12298e9c7f7c847
Amir Sharapov <amir@amirfounder.com>
2022-04-20 05:02:28 -0500

    some cool stuff happened tongith

--- better_requests/services/save_news_article.py
+++ better_requests/services/save_news_article.py
@@ -2,9 +2,7 @@ from typing import Dict
 
 from daos.internal import (
     NewsArticleHtmlDocumentRawRepository,
-    NewsArticleHtmlDocumentRawModel,
     NewsArticleHtmlDocumentIndexRepository,
-    NewsArticleHtmlDocumentIndexModel
 )
 
 from better_requests.services.base import BaseBetterRequestsService
@@ -13,28 +11,27 @@ from better_requests.services.base import BaseBetterRequestsService
 class SaveNewsArticle(BaseBetterRequestsService):
     def __init__(
             self,
-            document_repository: NewsArticleHtmlDocumentRawRepository,
+            raw_repository: NewsArticleHtmlDocumentRawRepository,
             index_repository: NewsArticleHtmlDocumentIndexRepository
     ):
-        self.document_repository = document_repository
+        self.raw_repository = raw_repository
         self.index_repository = index_repository
 
     def run(self, params: Dict):
-        document = NewsArticleHtmlDocumentRawModel()
+        document = self.raw_repository.create()
         document.contents = params.get('markup')
 
-        self.document_repository.save(document)
+        self.raw_repository.update(document)
 
-        index_item = NewsArticleHtmlDocumentIndexModel()
-
-        index_item.document_id = document.get_id()
-        index_item.document_format = self.document_repository.file_format.value
-        index_item.raw_version_document_path = str(document.get_path())
+        index_item = self.index_repository.create()
+        index_item.document_id = document.id
+        index_item.document_format = self.raw_repository.filetype
+        index_item.raw_version_document_path = str(document.path)
         index_item.is_raw_version_stored = True
         index_item.url = params.get('url')
         index_item.retrieved_from_web_at = params.get('timestamp')
 
-        self.index_repository.save(index_item)
+        self.index_repository.update(index_item)
 
         return {
             'status': 'SUCCESS'
