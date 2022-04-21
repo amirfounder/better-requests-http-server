78225acd4339c44442cb153ac36846fee9a5dd0d
Amir Sharapov <amir@amirfounder.com>
2022-04-21 00:32:36 -0500

    stuff

--- better_requests/server/services.py
+++ better_requests/server/services.py
@@ -3,7 +3,7 @@ from dependency_injection import service
 from http_server import HttpMethod
 
 from better_requests.server.adapter import BetterRequestEndpointAdapter as ServiceAdapter
-from better_requests.services.save_google_results import SaveGoogleSearchResults
+from better_requests.services.save_html_document import SaveHtmlDocument
 
 
 REPOSITORIES = [
@@ -15,7 +15,7 @@ for cls in REPOSITORIES:
     service(cls)
 
 SERVICES_PARAMS = [
-    ('/save-google-search-results', HttpMethod.POST, SaveGoogleSearchResults)
+    ('/save-html-document', HttpMethod.POST, SaveHtmlDocument)
 ]
 
 SERVICES = [ServiceAdapter(r, m, service(s)()) for r, m, s in SERVICES_PARAMS]
--- better_requests/services/save_html_document.py
+++ better_requests/services/save_html_document.py
@@ -8,7 +8,7 @@ from daos.internal import (
 from better_requests.services.base import BaseBetterRequestsService as Base
 
 
-class SaveGoogleSearchResults(Base):
+class SaveHtmlDocument(Base):
     def __init__(
             self,
             html_repository: RawHtmlDocumentRepository,
@@ -20,12 +20,11 @@ class SaveGoogleSearchResults(Base):
     def run(self, params: Dict):
         document = self.html_repository.create()
         document.contents = params.get('markup')
-
-        self.html_repository.update(document)
+        document.flush_contents()
 
         index_item = self.index_repository.create()
         index_item.document_id = document.id
-        index_item.document_format = self.html_repository.filetype
+        index_item.document_format = document.suffix
         index_item.raw_version_document_path = str(document.path)
         index_item.is_raw_version_stored = True
         index_item.url = params.get('url')
